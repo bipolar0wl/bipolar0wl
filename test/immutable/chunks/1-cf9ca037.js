e96dcce4ebfe8a6d1b682f79a099bfdb1eb50310
Ayaz Salikhov <89995511+bipolar0wl@users.noreply.github.com>
2022-11-25 00:05:08 +0300

    test

---
+++ test/immutable/chunks/1-cf9ca037.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-12f71db5.js";export{t as component};
---
+++ test/immutable/chunks/4-d31fb85d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/main/_page.svelte-bf9c4616.js";export{t as component};
