534b1b6e45eae836137b38accd8f50b1713b3df5
bipolar0wl <89995511+bipolar0wl@users.noreply.github.com>
2023-01-31 21:21:05 +0300

    images

--- _app/immutable/chunks/3-403bcaa8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/basket/_page.svelte-e35933cb.js";export{t as component};
---
+++ _app/immutable/chunks/4-9e318dc6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/main/_page.svelte-b4f47472.js";export{t as component};
