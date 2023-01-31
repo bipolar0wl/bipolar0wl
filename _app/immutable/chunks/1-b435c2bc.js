4f774143a63e6ff85de4f29d1f26c2276c556742
bipolar0wl <89995511+bipolar0wl@users.noreply.github.com>
2023-01-31 21:58:27 +0300

    backbutton

--- _app/immutable/chunks/1-b435c2bc.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-16d380bb.js";export{t as component};
--- _app/immutable/chunks/2-aaa77e8e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-a45610b4.js";export{t as component};
